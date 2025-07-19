89d82a5433899e60e77f149f5cc15871320af966
Ryann Carlos <ryanncarlos333@gmail.com>
2025-07-19 18:47:58 -0300

    sertver

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-// backend/src/server.ts
 import { dbConnect } from "./config/dbConnect"
 import app from "./app"
 import dotenv from "dotenv"
 dotenv.config()
 
-const PORT = process.env.PORT || 3333
+const PORT = Number(process.env.PORT) || 80 // 👈 Porta 80
 
 dbConnect().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Servidor rodando em http://localhost:${PORT}`)
+  app.listen(PORT, '0.0.0.0', () => { // 👈 Ouve todas as interfaces (não só localhost)
+    console.log(`🚀 Servidor rodando em http://0.0.0.0:${PORT}`)
   })
 })
